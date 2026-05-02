bdd35f558f6669d314207b810083c38e9d7946d1
Marwan Omar <mo9641133@gmail.com>
2026-05-02 18:02:23 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+  print('Hello, Marwan-Dev-HQ!');
+  print('Your Flutter journey starts here ');
+  
+  int logicLevel = 100;
+  if (logicLevel >= 100) {
+    print('Status: Pro Programmer ');
+  }
+}
