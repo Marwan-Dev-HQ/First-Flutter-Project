e5a5a197ed496eaa8abc0fd59309a82869063fb2
Marwan Omar <mo9641133@gmail.com>
2026-05-09 00:03:55 +0300

    Add career path switch statement in career_path.dart

---
+++ career_path.dart
@@ -0,0 +1,20 @@
+void main() {
+  String myPlan = "Manager";
+
+  switch (myPlan) {
+    case "Junior":
+      print("Study hard and build your GitHub");
+      break;
+    case "Mid-level":
+      print("Work on big projects and travel to Swiss");
+      break;
+    case "Senior":
+      print("Lead teams and make more money");
+      break;
+    case "Manager":
+      print("Private plane to Swiss and enjoy life");
+      break;
+    default:
+      print("Go now and study!");
+  }
+}
