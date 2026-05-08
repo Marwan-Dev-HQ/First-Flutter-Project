368b786eae37283d3588f7a1e6e9fa717182916b
Marwan Omar <mo9641133@gmail.com>
2026-05-08 13:28:48 +0300

    Track total fines collected for speeding
    Added totalMoney variable to track fines collected.

--- speed_radar.dart
+++ speed_radar.dart
@@ -2,11 +2,13 @@ void main() {
   List<int> carSpeeds = [80, 110, 140, 70, 200];
   int speedLimit = 140;
   int finesCount = 0;
+int totalMoney=0;
 
   for (var speed in carSpeeds) {
     if (speed >= speedLimit) {
       print("Over Speed: $speed");
       finesCount++;
+      totalMoney+=500;
     } else {
       print("Safe Speed: $speed");
     }
