e0f3c88e9e44490bfcaa59359b92415a01cd97ad
Marwan Omar <mo9641133@gmail.com>
2026-05-25 00:19:39 +0300

    Enhance createPlayer function with comments
    Added comments to clarify function usage and logic.

--- pubg_logic_challenge.dart
+++ pubg_logic_challenge.dart
@@ -1,17 +1,21 @@
 void main() {
+  // Calling the function with required arguments (Name and Weapon)
   createPlayer("Marwan", "M416");
   print("----------------------");
   createPlayer("Omar", "AKM");
 }
 
+// Function using an Optional Parameter [grenades] with a default value of 3
 void createPlayer(String playerName, String weapon, [int grenades = 3]) {
   print("name: $playerName");
   print("weapon: $weapon");
 
+  // Logic: Checking the weapon type to calculate the grenades bonus
   if (weapon == "M416") {
+    // M416 gets the default number of grenades
     print("\ngrenades number= $grenades"); 
   } else if (weapon == "AKM") {
+    // AKM gets a bonus of 2 extra grenades
     print("\ngrenades number= ${grenades + 2}"); 
   }
 }
-
