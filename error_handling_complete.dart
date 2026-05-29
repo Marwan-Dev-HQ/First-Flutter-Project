bcc0e03e675ead143f69f9bd1426245fd5953062
Marwan Omar <mo9641133@gmail.com>
2026-05-30 00:54:11 +0300

    Enhance error handling with explanatory comments
    Added comments to clarify error handling logic.

--- error_handling_complete.dart
+++ error_handling_complete.dart
@@ -1,11 +1,13 @@
 void main() {
   try {
+    // Forcing an actual exception using integer division by zero
     int number = 30 ~/ 0;
     print("result = $number");
   } catch (e) {
+    // Catching the exact exception if something goes wrong
     print("error is $e");
   } finally {
+    // This block will ALWAYS execute no matter what happens above
     print("always run the code");
   }
 }
-
