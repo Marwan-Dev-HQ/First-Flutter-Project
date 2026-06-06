e7e9231b8a195f4f7cccfc1d0ac18d31fa9522dc
Marwan Omar <mo9641133@gmail.com>
2026-06-07 02:56:55 +0300

    Implement CashBackCard class for cashback logic
    Add CashBackCard class extending Card with bonus attribute.

--- cashback_card_task.dart
+++ cashback_card_task.dart
@@ -1,3 +1,4 @@
+// Base class representing a standard card
 class Card {
   String cardNumber;
 
@@ -8,9 +9,11 @@ class Card {
   }
 }
 
+// Child class handling cashback logic and bonus rewards
 class CashBackCard extends Card {
   double bonus;
 
+  // Passing the card number to the super constructor
   CashBackCard({required String myNumber, required this.bonus}) : super(cardNumber: myNumber);
 
   @override
