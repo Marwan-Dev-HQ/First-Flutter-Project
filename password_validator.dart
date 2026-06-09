ec8373f67c02b3b5fef7d0c3335cd025846a3f75
Marwan Omar <mo9641133@gmail.com>
2026-06-10 01:12:37 +0300

    Implement main function for password validation demo
    Added main function to demonstrate password validation.

--- password_validator.dart
+++ password_validator.dart
@@ -1,10 +1,15 @@
 void main() {
+  // Create an instance of the validator class
   var validator = PasswordValidator();
+  
+  // Use direct invoke to check if the password meets the length requirement
   var isValid = validator("marwan12345");
+  
+  // Print the boolean result (true or false)
   print("Is password valid? $isValid");
 }
 
 class PasswordValidator {
+  // Callable function that returns true if password length is 8 or more
   bool call(String password) => password.length >= 8;
 }
-
