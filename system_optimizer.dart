1f6e4b176853b8ec4da46353375a2ffe3f343aef
Marwan Omar <mo9641133@gmail.com>
2026-06-12 03:26:23 +0300

    Implement SystemOptimizer with callable functionality
    Added a callable class for system optimizations.

--- system_optimizer.dart
+++ system_optimizer.dart
@@ -1,12 +1,17 @@
 void main() {
+  // Create an instance of the SystemOptimizer class
   var optimizer = SystemOptimizer();
 
+  // Using the callable class feature (Direct Invoke) to execute a command
   String executionResult = optimizer("adb shell pm compile -m speed -f");
 
+  // Print the final result to the console
   print(executionResult);
 }
 
+// A callable class designed to handle system optimizations
 class SystemOptimizer {
+  
+  // The magic 'call' method that allows the object to be executed like a function
   String call(String command) => "$command command has been executed successfully! ⚡";
 }
-
