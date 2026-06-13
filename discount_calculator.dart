01b40003a3ec871da21faf6de1da2da9d124cb6f
Marwan Omar <mo9641133@gmail.com>
2026-06-14 01:55:24 +0300

    Implement DiscountCalculator as a callable class
    Added a callable class to calculate discounts directly.

--- discount_calculator.dart
+++ discount_calculator.dart
@@ -1,10 +1,13 @@
 void main() {
+  // Create an instance of the Callable Class
   var calculate = DiscountCalculator();
 
+  // Call the object directly as a function inside print to avoid unused variables
   print("The price is 100, the discount 15, the final price is .... ${calculate(100, 15)}");
 }
 
+// A Callable Class that allows its instances to be called like a function
 class DiscountCalculator {
+  // The magic 'call' method that enables function-like behavior for the object
   double call(double price, double discount) => price - discount;
 }
-
