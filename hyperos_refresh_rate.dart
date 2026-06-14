8831336b9f9843564221370b6bf3cf2d9f58e155
Marwan Omar <mo9641133@gmail.com>
2026-06-14 16:23:09 +0300

    Refactor main function and update refresh rate mode
    Updated main function to test refresh rate in 'power Saving' mode and improved code comments.

--- hyperos_refresh_rate.dart
+++ hyperos_refresh_rate.dart
@@ -1,7 +1,13 @@
-void main(){
-  var tweak=HyperOsTweak();
-  print("The refresh Rate is :${tweak.getRefreshRate("performance")}Hz");
+void main() {
+  // Create an instance of the HyperOsTweak class
+  var tweak = HyperOsTweak();
+
+  // Test the function by passing a mode and printing the result directly
+  print("The refresh Rate is :${tweak.getRefreshRate("power Saving")}Hz");
 }
-class HyperOsTweak{
-  int getRefreshRate(String mode)=>mode=="performance"?120 :60;
+
+class HyperOsTweak {
+  // A clean Lambda Expression (Arrow Function) with a Ternary Operator to return 120 or 60
+  int getRefreshRate(String mode) => mode == "performance" ? 120 : 60;
 }
+
