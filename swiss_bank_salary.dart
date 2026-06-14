5c9abc834171421c1ddedf52967bdba7dbb68576
Marwan Omar <mo9641133@gmail.com>
2026-06-15 01:42:44 +0300

    Add comments and improve salary calculation logic
    Added comments for clarity and enhanced salary calculation.

--- swiss_bank_salary.dart
+++ swiss_bank_salary.dart
@@ -1,9 +1,13 @@
 void main() {
+  // Creating an instance of the SwissBank class for the future Senior Programmer
   var swissEngineer = SwissBank();
 
+  // Printing the total salary directly using string interpolation with CHF currency
   print("the total salary is = ${swissEngineer.calculateSalary("senior")} CHF");
 }
 
 class SwissBank {
+  // A perfect Lambda Expression using the arrow function to calculate salaries based on job title
   int calculateSalary(String title) => title == "senior" ? 9000 : 6000;
 }
+
