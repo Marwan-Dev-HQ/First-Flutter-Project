7c2bfa6b4f28022947007e09fa88d18bf8dbdeb3
Marwan Omar <mo9641133@gmail.com>
2026-06-15 14:54:55 +0300

    Add main function to handle name list operations
    Implement main function to initialize and manipulate a list of names.

---
+++ Fixed_list.dart
@@ -0,0 +1,7 @@
+void main() {
+List <String> nameList=["Marwan","Omar"];
+nameList.add("SwissMarwan");
+String uppernameList= nameList[0].toUpperCase();
+  print(nameList);
+  print(uppernameList);
+}
