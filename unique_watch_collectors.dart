f3d6bc03dcca9ab9acc7312fa4d00c22d71e0d70
Marwan Omar <mo9641133@gmail.com>
2026-06-18 16:19:46 +0300

    Add unique watch brands collection in Dart
    This Dart program demonstrates the use of a Set to store unique watch brands, ensuring no duplicates are added. It checks for the presence of a specific brand and prints all unique brands in the collection.

---
+++ unique_watch_collectors.dart
@@ -0,0 +1,12 @@
+void main(){
+  Set <String> watchBrands={};
+  watchBrands.add("Rolex");
+  watchBrands.add("Tag Heuer");
+  watchBrands.add("Rolex");
+  watchBrands.add("Patek Philippe");
+  if(watchBrands.contains("Tag Heuer")) {
+    print("Yes, Tag Heuer is in the collection!");
+  }
+     print("All unique brands: $watchBrands");
+     }
+     
