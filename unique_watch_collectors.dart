fc595af3b8a4f8aebcd31463aabb3dd8420c266e
Marwan Omar <mo9641133@gmail.com>
2026-06-18 16:27:50 +0300

    Enhance main function with comments on Set usage
    Added comments to explain Set operations and usage.

--- unique_watch_collectors.dart
+++ unique_watch_collectors.dart
@@ -1,12 +1,18 @@
-void main(){
-  Set <String> watchBrands={};
+void main() {
+  // Creating a Set of unique watch brands using curly braces {}
+  Set<String> watchBrands = {};
+
+  // Adding items to the Set using .add()
   watchBrands.add("Rolex");
   watchBrands.add("Tag Heuer");
-  watchBrands.add("Rolex");
+  watchBrands.add("Rolex"); // This duplicate will be automatically ignored!
   watchBrands.add("Patek Philippe");
-  if(watchBrands.contains("Tag Heuer")) {
+
+  // Checking if the Set contains "Tag Heuer" using .contains()
+  if (watchBrands.contains("Tag Heuer")) {
     print("Yes, Tag Heuer is in the collection!");
   }
-     print("All unique brands: $watchBrands");
-     }
-     
+
+  // Printing the final Set with only unique brands
+  print("All unique brands: $watchBrands");
+}
