4c256c5bb2eae55c6c014e3caeccab4002178121
Marwan Omar <mo9641133@gmail.com>
2026-06-19 00:58:47 +0300

    Document Set operations in github_tags_tracker.dart
    Added comments to clarify the functionality of Set operations.

--- github_tags_tracker.dart
+++ github_tags_tracker.dart
@@ -1,18 +1,22 @@
 void main() {
+  // Creating a Set of unique project tags using curly braces {}
   Set<String> projectsTags = {};
 
+  // Adding tags to the Set using .add()
   projectsTags.add("Flutter");
   projectsTags.add("Dart");
   projectsTags.add("Hyper os");
-  projectsTags.add("Flutter");
+  projectsTags.add("Flutter"); // This duplicate will be automatically ignored!
   projectsTags.add("Gaming");
 
+  // Checking if the Set contains "Dart" using .contains()
   if (projectsTags.contains("Dart")) {
     print("Awesome! Dart is ready for Flutter!");
   }
 
+  // Removing "Gaming" tag from the Set using .remove()
   projectsTags.remove("Gaming");
 
+  // Printing the final Set with unique tags left
   print("Final unique tags: $projectsTags");
 }
-
