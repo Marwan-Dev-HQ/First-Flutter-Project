dde5574b00ef4010860ef471a8414089eeada1fe
Marwan Omar <mo9641133@gmail.com>
2026-06-20 00:49:19 +0300

    Add comments to watch details tracker
    Added comments to explain the functionality of the code.

--- watch_details_tracker.dart
+++ watch_details_tracker.dart
@@ -1,14 +1,17 @@
 void main() {
+  // Creating a Map to store watch brands and their country of origin
   Map<String, String> watchDetails = {};
 
+  // Adding watch details using Key: Value pairs
   watchDetails["Rolex"] = "Switzerland";
   watchDetails["Tag Heuer"] = "Switzerland";
   watchDetails["Seiko"] = "Japan";
 
+  // Checking if the Map contains "Rolex" using .containsKey()
   if (watchDetails.containsKey("Rolex")) {
     print("Yes, Rolex is in the tracker!");
   }
 
+  // Printing the complete map with all watch details
   print("All watch details: $watchDetails");
 }
-
