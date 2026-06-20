ed05e39e9e5a9cc56a478e75419161975b3d2bb1
Marwan Omar <mo9641133@gmail.com>
2026-06-20 16:14:04 +0300

    Update global_brands_tracker.dart
    add special comments

--- global_brands_tracker.dart
+++ global_brands_tracker.dart
@@ -1,10 +1,17 @@
-void main(){
-  Map <String,String> globalBrands={};
-  globalBrands["Mercedes"]="Stuttgart";
-  globalBrands["Apple"]="California";
-  globalBrands["Nike"]="oregon";
-  if(globalBrands.containsKey("Apple")) {
+void main() {
+  // Creating a Map for global brands and their headquarters
+  Map<String, String> globalBrands = {};
+
+  // Adding global brands using bracket notation []
+  globalBrands["Mercedes"] = "Stuttgart";
+  globalBrands["Apple"] = "California";
+  globalBrands["Nike"] = "Oregon";
+
+  // Checking if Apple exists in the map
+  if (globalBrands.containsKey("Apple")) {
     print("Apple headquarters is in the tracker!");
   }
+
+  // Printing the complete map details
   print(globalBrands);
-}    
+}
