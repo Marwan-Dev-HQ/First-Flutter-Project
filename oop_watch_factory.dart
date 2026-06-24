2037b3e910fb8864b725830585c50a79bef17738
Marwan Omar <mo9641133@gmail.com>
2026-06-24 03:25:15 +0300

    Instantiate LuxuryWatch and display details
    Added instances of LuxuryWatch and method calls to display details.

--- oop_watch_factory.dart
+++ oop_watch_factory.dart
@@ -1,21 +1,26 @@
 void main() {
+  // Creating instances of the LuxuryWatch class using constructors
   LuxuryWatch watchOne = LuxuryWatch("Tag Heuer", 4500, "USD");
   LuxuryWatch watchTwo = LuxuryWatch("Jacob & Co", 150000, "USD");
 
   print("--- System Luxury Watch Database ---");
+  
+  // Calling the method to display details for each watch instance
   watchOne.printDetails();
   watchTwo.printDetails();
 }
 
+// Defining the class blueprint for luxury watches
 class LuxuryWatch {
   String brand;
   double price;
   String currency;
 
+  // Generative constructor to initialize fields immediately
   LuxuryWatch(this.brand, this.price, this.currency);
 
+  // Method to print the watch information nicely
   void printDetails() {
     print("Brand: $brand | Price: $price $currency");
   }
 }
-
