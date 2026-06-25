40d5cce26c97eaa988012c1cc456aaafcc56d154
Marwan Omar <mo9641133@gmail.com>
2026-06-25 22:50:57 +0300

    Refactor ElectricCar and Vehicle classes
    Refactored ElectricCar and Vehicle classes for improved readability and structure. Enhanced displayInfo method and constructor syntax.

--- Swiss_gorgeous Cars.dart
+++ Swiss_gorgeous Cars.dart
@@ -1,24 +1,43 @@
 void main() {
-  var swissCar=ElectricCar(batteryCapacity:100,brand:"porsche",price:150000);
+  // Creating an instance of the child class (ElectricCar) with all required data
+  var swissCar = ElectricCar(
+    batteryCapacity: 100, 
+    brand: "porsche", 
+    price: 150000
+  );
+  
+  // Calling the overridden method which executes both parent and child logic
   swissCar.displayInfo();
 }
 
-class Vehicle{
+// Parent Class (Superclass)
+class Vehicle {
   String? brand;
   double? price;
-  Vehicle({ this.brand,this.price});
-  void displayInfo(){
-  print("car brand is $brand,the price is $price");
+  
+  // Constructor using shorthand syntax to initialize brand and price
+  Vehicle({this.brand, this.price});
+  
+  // Basic method to print parent properties
+  void displayInfo() {
+    print("car brand is $brand, the price is \$price");
   }
 }
-class ElectricCar extends Vehicle{
-  int batteryCapacity;
+
+// Child Class (Subclass) inheriting from Vehicle
+class ElectricCar extends Vehicle {
+  int batteryCapacity; // Exclusive property for the child class
   
-  ElectricCar({required this.batteryCapacity,String? brand,double? price}): super(brand:brand,price:price);
-  @override
-  void displayInfo(){
-    super.displayInfo();
-    print("The battery amount is : ${batteryCapacity}%!");
-  
+  // Constructor initializing child property and forwarding brand/price to the parent via 'super'
+  ElectricCar({
+    required this.batteryCapacity, 
+    String? brand, 
+    double? price
+  }) : super(brand: brand, price: price);
+      
+  @override // Indicates we are overriding the parent's method
+  void displayInfo() {
+    super.displayInfo(); // Calling the parent's displayInfo() to print brand and price
+    print("The battery amount is : ${batteryCapacity}%!"); // Printing the exclusive child data
   }
 }
