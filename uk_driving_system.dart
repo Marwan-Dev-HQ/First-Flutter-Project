32716c693f0836805387e36c0680a82d401b3c40
Marwan Omar <mo9641133@gmail.com>
2026-06-29 00:37:51 +0300

    Add methods to display traffic rules and check driver status

--- uk_driving_system.dart
+++ uk_driving_system.dart
@@ -4,6 +4,7 @@ class UKDriver {
 
   UKDriver({required this.name, required this.age});
 
+  // Display general traffic rules in the UK
   void displayRules() {
     print('UK Driving Rule: Keep left and respect speed limits!');
   }
@@ -15,15 +16,16 @@ class FullLicense extends UKDriver {
   FullLicense({required String name, required int age, required this.licenseNumber})
       : super(name: name, age: age);
 
+  // Validate the driver status
   void checkStatus() {
     print('Driver $name (ID: $licenseNumber) is legally allowed to drive in London.');
   }
 }
 
 void main() {
+  // Create an instance of a driver with a full license
   var driver = FullLicense(name: 'Marwan Omar', age: 18, licenseNumber: 'UK-998877');
 
   driver.displayRules();
   driver.checkStatus();
 }
-
