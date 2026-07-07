9138ae34c6ec4c07731ce68fa773f06cccc83346
Marwan Omar <mo9641133@gmail.com>
2026-07-08 00:34:14 +0300

    Implement optional discount in ShippingManager
    Added optional discount parameter to shipping calculations and updated order processing.

--- shipping_manager.dart
+++ shipping_manager.dart
@@ -1,12 +1,14 @@
-
+// class to handle shipping calculations
 class ShippingManager {
   final double baseRate = 50.0;
 
+  // using {} here to make the discount parameter optional
   double calculateShipping(double weight, {double discount = 0.0}) {
     double total = weight * baseRate;
     return total - discount;
   }
 
+  // function to print order details on screen
   void processOrder(String customerName, double weight, {double discount = 0.0}) {
     double finalCost = calculateShipping(weight, discount: discount);
     
@@ -20,8 +22,9 @@ void main() {
   print('Testing shipping system');
   var shipping = ShippingManager();
 
+  // calling without discount
   shipping.processOrder('Marwan', 4.5);
 
+  // calling with the optional discount parameter
   shipping.processOrder('Ziad', 10.0, discount: 25.0);
 }
-
