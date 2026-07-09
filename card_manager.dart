ca04888ddd7e072d54e8bf8eafaf9b462a6a7538
Marwan Omar <mo9641133@gmail.com>
2026-07-10 01:06:24 +0300

    Improve CardManager with comments and initial values
    Added comments for clarity and initialized card codes.

--- card_manager.dart
+++ card_manager.dart
@@ -1,7 +1,9 @@
 class CardManager {
   void manageCards() {
+    // keeping it fixed to save memory
     var codes = List<String>.filled(3, 'EMPTY');
 
+    // adding some initial data
     codes[0] = 'ABC12';
     codes[1] = 'XYZ99';
 
