7982713713a395abb48bd327189c5ee4b4aaab28
Marwan Omar <mo9641133@gmail.com>
2026-07-12 04:28:52 +0300

    Enhance comments in modifyFactoryAssets method
    Added comments to clarify the operations on the tools set.

--- set_operations.dart
+++ set_operations.dart
@@ -1,11 +1,18 @@
 class SetOperations {
   void modifyFactoryAssets() {
+    // Our main tools list
     var tools = <String>{'Wrench', 'Hammer'};
 
+    // Just adding a new tool
     tools.add('Pliers');
+    
+    // Won't be added because it's a duplicate
     tools.add('Wrench'); 
+    
+    // Removing what we don't need
     tools.remove('Hammer');
 
+    // Printing the final list and checking for the hammer
     print(tools);
     print(tools.contains('Hammer'));
   }
@@ -15,4 +22,3 @@ void main() {
   var operations = SetOperations();
   operations.modifyFactoryAssets();
 }
-
