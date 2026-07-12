98fddd3ce11e9b490fb8e1e3c8202c4bea3b9fa6
Marwan Omar <mo9641133@gmail.com>
2026-07-13 00:29:00 +0300

    Update laptop_specs.dart

--- laptop_specs.dart
+++ laptop_specs.dart
@@ -1,13 +1,16 @@
 class LaptopSpecs {
   void showSpecs() {
+    // Key and Value pairs for the setup
     var specs = <String, String>{
       'Laptop': 'Lenovo LOQ',
       'Switch': 'Red Switch',
       'OS': 'HyperOS Connected'
     };
 
+    // Printing the whole map
     print(specs);
 
+    // Fetching a specific value using its key
     print(specs['Laptop']);
   }
 }
@@ -16,4 +19,3 @@ void main() {
   var mySetup = LaptopSpecs();
   mySetup.showSpecs();
 }
-
