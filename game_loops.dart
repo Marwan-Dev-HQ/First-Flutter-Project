2ae05720e427935c5061093597629545baadd7d4
Marwan Omar <mo9641133@gmail.com>
2026-07-15 02:13:34 +0300

    Update game_loops.dart

--- game_loops.dart
+++ game_loops.dart
@@ -2,6 +2,7 @@ class GameLoops {
   void runGameplay() {
     int stamina = 30;
 
+    // Running as long as we have stamina left
     while (stamina > 0) {
       print('Sprinting... Stamina left: $stamina%');
       stamina -= 10;
@@ -11,6 +12,7 @@ class GameLoops {
 
     int saveAttempts = 0;
 
+    // Saving once immediately, then checking if we need to try again
     do {
       print('Autosaving game progress...');
       saveAttempts--;
