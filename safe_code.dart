56818418a99bfde2fd10cd88f3bf65e8e111d9ed
Marwan Omar <mo9641133@gmail.com>
2026-07-16 00:21:22 +0300

    Add null safety comments and print statusMessage

--- safe_code.dart
+++ safe_code.dart
@@ -1,11 +1,15 @@
 class SafeCode {
   void checkStatus() {
+    // This variable CANNOT be null. Dart protects it!
     String username = 'Marwan';
+    
+    // The '?' means this variable CAN be null or empty
     String? statusMessage;
 
     print(username);
-    print(statusMessage);
+    print(statusMessage); // Will print: null (without crashing!)
 
+    // Now we give it a value, completely safe!
     statusMessage = 'Active';
     print(statusMessage);
   }
@@ -15,4 +19,3 @@ void main() {
   var user = SafeCode();
   user.checkStatus();
 }
-
