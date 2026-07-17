3dc54276f7794f58375edb728661cd32f915b1dd
Marwan Omar <mo9641133@gmail.com>
2026-07-17 20:59:18 +0300

    Fix initial total commits for Marwan's profile
    Updated Marwan's total commits from 193 to 192 before adding a new commit.

--- streak_tracker.dart
+++ streak_tracker.dart
@@ -1,14 +1,17 @@
+// Hey! This class is just to keep an eye on our daily hustle
 class CommitTracker {
   final String coderName;
   int totalCommits;
   final String currentTarget;
 
+  // Setting up the basic info we need
   CommitTracker({
     required this.coderName,
     required this.totalCommits,
     this.currentTarget = 'Flutter & LOQ Laptop',
   });
 
+  // This is where the magic happens and the numbers go up
   void addNewCommit() {
     totalCommits++;
     print('--- Achievement Unlocked 🟢 ---');
@@ -21,7 +24,9 @@ class CommitTracker {
 }
 
 void main() {
-  var marwanStreak = CommitTracker(coderName: 'Marwan', totalCommits: 193);
+  // Creating Marwan's profile and setting the starting point
+  var marwanStreak = CommitTracker(coderName: 'Marwan', totalCommits: 192);
+  
+  // Boom! Let's hit that commit 193 right now
   marwanStreak.addNewCommit();
 }
-
